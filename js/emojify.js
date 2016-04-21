467a558042efc59c3313d966bbfffd8ffa8de4b5
Ítalo Vicente <itinhokannario2010@gmail.com>
2016-04-21 01:05:18 -0300

    Sorrisos e pessoas completo
    primeiro bloco pronto

--- js/emojify.js
+++ js/emojify.js
@@ -119,6 +119,122 @@
                     /* \ud83d\ude38 */ smile_cat: /\\ud83d\\ude38/, 
                     /* \ud83d\ude40 */ scream_cat: /\\ud83d\\ude40/,
                     /* \ud83d\udc4b */ wave: /\\ud83d\\udc4b/,
+                    /* \ud83d\udc4d */ legal: /\\ud83d\\udc4d/,
+                    /* \ud83d\udc4e */ nao_legal: /\\ud83d\\udc4e/,
+                    /* \ud83d\udc4a */ facepunch: /\\ud83d\\udc4a/,
+                    /* \u270a */       fist: /\\u270a/,
+                    /* \u270c\ufe0f */ v: /\\u270c\\ufe0f/,
+                    /* \ud83d\udc4c */ ok_hand: /\\ud83d\\udc4c/,
+                    /* \u270b */       raised_hand: /\\u270b/,
+                    /* \ud83d\udc50 */ open_hands: /\\ud83d\\udc50/,
+                    /* \ud83d\udcaa */ muscle: /\\ud83d\\udcaa/,
+                    /* \ud83d\ude4f */ pray: /\\ud83d\\ude4f/,
+                    /* \u261d\ufe0f */ point_up: /\\u261d\\ufe0f/,
+                    /* \ud83d\udc46 */ point_up_2: /\\ud83d\\udc46/,
+                    /* \ud83d\udc47 */ point_down: /\\ud83d\\udc47/,
+                    /* \ud83d\udc48 */ point_left: /\\ud83d\\udc48/,
+                    /* \ud83d\udc49 */ point_right: /\\ud83d\\udc49/,
+                    /* \ud83d\udd95 */ fu: /\\ud83d\\udd95/,
+                    /* \ud83d\udd90 */ esse_nao_tem_imagem15: /\\ud83d\\udd90/,
+                    /* \ud83e\udd18 */ metal: /\\ud83e\\udd18/,
+                    /* \ud83d\udd96 */ esse_nao_tem_imagem16: /\\ud83d\\udd96/,
+                    /* \u270d */       esse_nao_tem_imagem17: /\\u270d/,
+                    /* \ud83d\udc85 */ nail_care: /\\ud83d\\udc85/,
+                    /* \ud83d\udc44 */ lips: /\\ud83d\\udc44/,
+                    /* \ud83d\udc45 */ tongue: /\\ud83d\\udc45/,
+                    /* \ud83d\udc42 */ ear: /\\ud83d\\udc42/,
+                    /* \ud83d\udc43 */ nose: /\\ud83d\\udc43/,
+                    /* \ud83d\udc41 */ esse_nao_tem_imagem18: /\\ud83d\\udc41/,
+                    /* \ud83d\udc40 */ eyes: /\\ud83d\\udc40/,
+                    /* \ud83d\udc64 */ bust_in_silhouette: /\\ud83d\\udc64/,
+                    /* \ud83d\udc65 */ busts_in_silhouette2: /\\ud83d\\udc65/,
+                    /* \ud83d\udde3 */ esse_nao_tem_imagem19: /\\ud83d\\udde3/,
+                    /* \ud83d\udc76 */ baby: /\\ud83d\\udc76/,
+                    /* \ud83d\udc66 */ person_with_blond_hair: /\\ud83d\\udc66/,
+                    /* \ud83d\udc67 */ girl: /\\ud83d\\udc67/,
+                    /* \ud83d\udc68 */ man: /\\ud83d\\udc68/,
+                    /* \ud83d\udc69 */ woman: /\\ud83d\\udc69/,
+                    /* \ud83d\udc71 */ boy: /\\ud83d\\udc71/,
+                    /* \ud83d\udc74 */ older_man: /\\ud83d\\udc74/,
+                    /* \ud83d\udc75 */ older_woman: /\\ud83d\\udc75/,
+                    /* \ud83d\udc72 */ man_with_gua_pi_mao: /\\ud83d\\udc72/,
+                    /* \ud83d\udc73 */ man_with_turban: /\\ud83d\\udc73/,
+                    /* \ud83d\udc6e */ cop: /\\ud83d\\udc6e/,
+                    /* \ud83d\udc77 */ construction_worker: /\\ud83d\\udc77/,
+                    /* \ud83d\udc82 */ guardsman: /\\ud83d\\udc82/,
+                    /* \ud83d\udd75 */ esse_nao_tem_imagem20: /\\ud83d\\udd75/,
+                    /* \ud83c\udf85 */ santa: /\\ud83c\\udf85/,
+                    /* \ud83d\udc7c */ angel: /\\ud83d\\udc7c/,
+                    /* \ud83d\udc78 */ princess: /\\ud83d\\udc78/,
+                    /* \ud83d\udc70 */ bride_with_veil: /\\ud83d\\udc70/,
+                    /* \ud83d\udeb6 */ walking: /\\ud83d\\udeb6/,
+                    /* \ud83c\udfc3 */ running: /\\ud83c\\udfc3/,
+                    /* \ud83d\udc83 */ dancer: /\\ud83d\\udc83/,
+                    /* \ud83d\udc6f */ dancers: /\\ud83d\\udc6f/,
+                    /* \ud83d\udc6b */ couple: /\\ud83d\\udc6b/,
+                    /* \ud83d\udc6c */ two_men_holding_hands: /\\ud83d\\udc6c/,
+                    /* \ud83d\udc6d */ two_women_holding_hands: /\\ud83d\\udc6d/,
+                    /* \ud83d\ude47 */ bow: /\\ud83d\\ude47/,
+                    /* \ud83d\udc81 */ information_desk_person: /\\ud83d\\udc81/,
+                    /* \ud83d\ude45 */ no_good: /\\ud83d\\ude45/,
+                    /* \ud83d\ude46 */ ok_woman: /\\ud83d\\ude46/,
+                    /* \ud83d\ude4b */ raising_hand: /\\ud83d\\ude4b/,
+                    /* \ud83d\ude4e */ person_with_pouting_face: /\\ud83d\\ude4e/,
+                    /* \ud83d\ude4d */ person_frowning: /\\ud83d\\ude4d/,
+                    /* \ud83d\udc87 */ haircut: /\\ud83d\\udc87/,
+                    /* \ud83d\udc86 */ massage: /\\ud83d\\udc86/,
+                    /* \ud83d\udc91 */ couple_with_heart: /\\ud83d\\udc91/,
+                    /* \ud83d\udc8f */ couplekiss: /\\ud83d\\udc8f/,
+                    /* \u200d\u2764 */ family: /\\u200d\\u2764/,
+                    /* \ud83d\udc6a */ womans_clothes: /\\ud83d\\udc6a/,
+                    /* \ud83d\udc55 */ shirt: /\\ud83d\\udc55/,
+                    /* \ud83d\udc56 */ jeans: /\\ud83d\\udc56/,
+                    /* \ud83d\udc54 */ esse_nao_tem_imagem21: /\\ud83d\\udc54/,
+                    /* \ud83d\udc57 */ dress: /\\ud83d\\udc57/,
+                    /* \ud83d\udc59 */ bikini: /\\ud83d\\udc59/,
+                    /* \ud83d\udc58 */ kimono: /\\ud83d\\udc58/,
+                    /* \ud83d\udc84 */ lipstick: /\\ud83d\\udc84/,
+                    /* \ud83d\udc8b */ kiss: /\\ud83d\\udc8b/,
+                    /* \ud83d\udc63 */ feet: /\\ud83d\\udc63/,
+                    /* \ud83d\udc60 */ high_heel: /\\ud83d\\udc60/,
+                    /* \ud83d\udc61 */ sandal: /\\ud83d\\udc61/,
+                    /* \ud83d\udc62 */ boot: /\\ud83d\\udc62/,
+                    /* \ud83d\udc5e */ mans_shoe: /\\ud83d\\udc5e/,
+                    /* \ud83d\udc5f */ shoe: /\\ud83d\\udc5f/,
+                    /* \ud83d\udc52 */ womans_hat: /\\ud83d\\udc52/,
+                    /* \ud83c\udfa9 */ tophat: /\\ud83c\\udfa9/,
+                    /* \ud83c\udf93 */ mortar_board: /\\ud83c\\udf93/,
+                    /* \ud83d\udc51 */ crown: /\\ud83d\\udc51/,
+                    /* \u26d1 */       esse_nao_tem_imagem22: /\\u26d1/,
+                    /* \ud83c\udf92 */ school_satchel: /\\ud83c\\udf92/,
+                    /* \ud83d\udc5d */ pouch: /\\ud83d\\udc5d/,
+                    /* \ud83d\udc5b */ purse: /\\ud83d\\udc5b/,
+                    /* \ud83d\udc5c */ handbag: /\\ud83d\\udc5c/,
+                    /* \ud83d\udcbc */ briefcase: /\\ud83d\\udcbc/,
+                    /* \ud83d\udc53 */ eyeglasses: /\\ud83d\\udc53/,
+                    /* \ud83d\udd76 */ esse_nao_tem_imagem23: /\\ud83d\\udd76/,
+                    /* \ud83d\udc8d */ ring: /\\ud83d\\udc8d/,
+                    /* \ud83c\udf02 */ closed_umbrella: /\\ud83c\\udf02/,
+
+                    // Emojis Animais e Natureza
+
+                    /* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+
+
 
                      // Emojis Default Facebook
 
